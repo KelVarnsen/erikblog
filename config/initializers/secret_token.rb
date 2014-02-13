33b1109839628134659c085c9f534f9c2859e42f
Erik Anderson <ecanderson90@gmail.com>
2014-02-13 15:01:27 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ErikBlog::Application.config.secret_key_base = 'a7a9eae4884e604ff76701820b6ae9346e84fe1e61ddcf153280e9b1e004b18f4ba539ff5b46c03395670fff1dc218e9d3c11ce382c7c2b12a0600cb9578aaa1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ErikBlog::Application.config.session_store :cookie_store, key: '_ErikBlog_session'
